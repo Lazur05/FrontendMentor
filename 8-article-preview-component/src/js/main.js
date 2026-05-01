23f367ba623a702342c3fdda0a29356cb2b6e4bf
Lazur05 <maciejkoziel23@gmail.com>
2026-05-01 10:14:02 +0200

    Refactor share button functionality to use state classes for improved styling and interactivity

--- 8-article-preview-component/src/js/main.js
+++ 8-article-preview-component/src/js/main.js
@@ -3,17 +3,9 @@ const shareIcon = document.querySelector(".profile-share-icon");
 const shareMenu = document.querySelector(".share-menu");
 
 shareBtn.addEventListener("click", () => {
-  shareMenu.classList.toggle("invisible");
   shareMenu.classList.toggle("menu-reveal-open");
-  if (shareBtn.classList.contains("bg-light-grayish-blue")) {
-    shareBtn.classList.remove("bg-light-grayish-blue");
-    shareBtn.classList.add("bg-grayish-blue");
-    shareIcon.classList.remove("fill-dark-blue");
-    shareIcon.classList.add("fill-light-grayish-blue");
-  } else {
-    shareBtn.classList.remove("bg-grayish-blue");
-    shareBtn.classList.add("bg-light-grayish-blue");
-    shareIcon.classList.remove("fill-light-grayish-blue");
-    shareIcon.classList.add("fill-dark-blue");
-  }
+  shareBtn.classList.toggle("btn-active");
+  shareBtn.classList.toggle("btn-inactive");
+  shareIcon.classList.toggle("icon-active");
+  shareIcon.classList.toggle("icon-inactive");
 });
